4c688fd546f467f245aeddc2a5d86a0dd5384e73
Tyler McDonough <tcmcd1337@gmail.com>
2017-08-05 14:53:21 -0400

    TriggerVolume +Map Actors

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+#include "Gameframework/Actor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void UOpenDoor::OpenDoor()
+{
+	
+	//Find the owning actor
+	AActor* Owner = GetOwner();
+
+	//Create a Rotator
+	FRotator NewRotation = FRotator(0.0f, 90.0f, 0.0f);
+	//Set Actor rotation +90 degrees ---- ^^
+	Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	// Poll the Trigger Volume every frame
+	// If the Actor is authorized to Open is in the Volume
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{//then open door
+		OpenDoor();
+	}
+	
+}
+
--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "Gameframework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -21,8 +22,9 @@ void UPositionReporter::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
-	UE_LOG(LogTemp, Warning, TEXT("POSITION REPORTER REPORTING on unreal Object and sam is here :)\n"));
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()-> GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s :)\n"), *ObjectName, *ObjectPos);
 	
 }
 
